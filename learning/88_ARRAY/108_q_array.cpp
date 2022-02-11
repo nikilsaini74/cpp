c855df63692481bc1e6691395181bfa3b4314f76
Anjali-60 <vanjalivrm@gmail.com>
2022-02-11 19:39:14 +0530

    .

---
+++ learning/88_ARRAY/108_q_array.cpp
@@ -0,0 +1,27 @@
+// read 100 integers and display all pairs of elements whose sum is 50.
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+    int arr[100];
+    srand(time(0));
+    for (int i = 0; i < 100; i++)
+    {
+        arr[i] = (rand()) % 100;
+    }
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = i + 1; j < 100; j++)
+        {
+            if (arr[i] + arr[j] == 50)
+            {
+                cout << i << " + " << j << " = 50";
+                cout << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
