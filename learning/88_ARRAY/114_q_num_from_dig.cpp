b6db9d0024e45c0cfdf6cf88538abd1622f19b99
Anjali-60 <vanjalivrm@gmail.com>
2022-05-08 22:05:42 +0530

    .

--- learning/88_ARRAY/114_q_num_from_dig.cpp
+++ learning/88_ARRAY/114_q_num_from_dig.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 int main()
 {
-    int arr[5];
+    int arr[5], num = 0;
     for (int i = 0; i < 5; i++)
     {
         cin >> arr[i];
     }
-    for(int i=0;i<5;i++)
+    for (int i = 0; i < 5; i++)
     {
-    
+        num = (num * 10) + arr[i];
     }
-
+    cout << num;
     return 0;
 }
