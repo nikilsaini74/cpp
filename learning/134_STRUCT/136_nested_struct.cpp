9178ce1a0b6fa614d1a08bf44627af0e9a8b9814
Anjali-60 <vanjalivrm@gmail.com>
2022-06-11 19:13:31 +0530

    ;

--- learning/134_STRUCT/136_nested_struct.cpp
+++ learning/134_STRUCT/136_nested_struct.cpp
@@ -4,15 +4,29 @@ int main()
 {
     struct NAME
     {
-        char first_name;
-        char last_name;
+        char first_name[20];
+        char last_name[20];
     };
     struct student
     {
         int roll_no;
-        NAME name;
-        
-    } 
+        struct NAME name;
+        float fees;
+    };
+    student stud1;
+    cout << "\n ENter roll no = ";
+    cin >> stud1.roll_no;
+    cout << "\n Enter name = ";
+    cin.ignore();
+    cin.getline(stud1.name.first_name, 20);
+    cin.ignore();
+    cin.getline(stud1.name.last_name, 20);
+    cout << "\n Enter fees = ";
+    cin >> stud1.fees;
+    cout << "\n STUDENT'S DETAILS" << endl;
+    cout << stud1.roll_no << endl;
+    cout << stud1.name.first_name << " " << stud1.name.last_name << endl;
+    cout << stud1.fees;
 
     return 0;
 }
