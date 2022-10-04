678505a851b603d31cac4daec6008f4c1957daa6
Anjali-60 <vanjalivrm@gmail.com>
2022-10-04 18:37:37 +0530

    linked list

--- learning/B_LINKED LIST/4_doubly_linked_list.cpp
+++ learning/B_LINKED LIST/4_doubly_linked_list.cpp
@@ -9,7 +9,7 @@ public:
     node *next;
     node *prev;
 
-    node (int val) //constructor
+    node(int val) // constructor
     {
         data = val;
         next = NULL;
@@ -17,9 +17,49 @@ public:
     }
 };
 
-void 
- int main()
+void insertAtHead(node *&head, int val)
 {
+    node *n = new node(val);
+    n->next = head;
+    head->prev = n;
+    head = n;
+}
 
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    node *temp = head;
+
+    if (head == NULL)
+    {
+        n->next = head;
+        head = n;
+        return;
+    }
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL\n";
+}
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    display(head);
     return 0;
 }
