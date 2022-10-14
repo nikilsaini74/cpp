ca933ae7edcd71c07fa48d0f07110c7b1fa764e4
Anjali-60 <vanjalivrm@gmail.com>
2022-10-14 23:18:02 +0530

    linked list

--- learning/B_LINKED LIST/5_append_last_k_ele.cpp
+++ learning/B_LINKED LIST/5_append_last_k_ele.cpp
@@ -33,9 +33,62 @@ void insertAtTail(node *&head, int val)
     temp->next = n;
 }
 
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int length(node *head)
+{
+    int l = 0;
+    node *temp = head;
+    while (temp != NULL)
+    {
+        l++;
+        temp = temp->next;
+    }
+    return l;
+}
+
+node *kappend(node *&head, int k)
+{
+    node *newhead;
+    node *newtail;
+    node *temp = head;
+
+    int l = length(head);
+    int count = 1;
+    k = k % l;
+    while (temp->next != NULL)
+    {
+        if (count == l - k)
+            newtail = temp;
+        if (count == l - k + 1)
+            newhead = temp;
+        temp = temp->next;
+        count++;
+    }
+    newtail->next = NULL;
+    temp->next = head;
+    return newhead;
+}
+
 int main()
 {
-    
-    
+    node *head = NULL;
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    for (int i = 0; i < 6; i++)
+        insertAtTail(head, arr[i]);
+    display(head);
+    cout << endl;
+    node *newhead = kappend(head, 4);
+    display(newhead);
+
     return 0;
 }
