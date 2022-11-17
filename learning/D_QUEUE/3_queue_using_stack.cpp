c7df6708686911a1d2316462c460e324ad06cafe
Anjali-60 <vanjalivrm@gmail.com>
2022-11-18 01:18:59 +0530

    queue

---
+++ learning/D_QUEUE/3_queue_using_stack.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class queue
+{
+    stack<int> st;
+
+public:
+    void push(int x)
+    {
+        st.push(x);
+        return;
+    }
+    int pop()
+    {
+        if (st.empty())
+        {
+            cout << "Queue is empty" << endl;
+        }
+        int x = st.top();
+        st.pop();
+        if (st.empty())
+            return x;
+        int item = pop();
+         st.push(x);
+        return item;
+    }
+    bool empty()
+    {
+        if (st.empty())
+            return true;
+        return false;
+    }
+};
+int main()
+{
+    queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    cout << q.pop() << endl;
+    q.pop();
+    cout << q.pop() << endl;
+    cout << q.empty() << endl;
+    return 0;
+}
