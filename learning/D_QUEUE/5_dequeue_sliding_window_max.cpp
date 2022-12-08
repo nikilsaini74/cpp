69048bf1017f2ea069d3858c3cf4ebd01cc31bdf
Anjali-60 <vanjalivrm@gmail.com>
2022-12-08 23:20:25 +0530

    deque

--- learning/D_QUEUE/5_dequeue_sliding_window_max.cpp
+++ learning/D_QUEUE/5_dequeue_sliding_window_max.cpp
@@ -1,24 +1,35 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 #include <iostream>
+#include <deque>
 using namespace std;
+
+void printMax(int a[], int n, int k)
+{
+    deque<int> q;
+    int i;
+    for (i = 0; i < k; ++i)
+    {
+        while ((!q.empty()) && a[i] >= a[q.back()])
+            q.pop_back();
+        q.push_back(i);
+    }
+
+    for (; i <= n; ++i)
+    {
+        cout << a[q.front()] << " ";
+     //   while (!q.empty() && q.front() <= i - k)
+            q.pop_front();
+        while (!q.empty() && a[i] >= a[q.back()])
+            q.pop_back();
+        q.push_back(i);
+    }
+    cout << a[q.front()] << " ";
+}
 
 int main()
 {
-    int n, k;
-    cin >> n >> k;
-    vector<int> a(n);
-    for (auto &i : a)
-        cin >> i;
-
-    deque<int> q;
-    vector<int> ans;
-
-    for(int i=0;i<k;i++)
-{
-    while(!q.empty() and a[i] > q.)
-}    
-    multiset<int,greater<int>> s;
-
-
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int k = 4;
+    printMax(arr, 9, k);
     return 0;
 }
