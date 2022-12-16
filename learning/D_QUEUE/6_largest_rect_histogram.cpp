237edf456379aab636b556fd650837088feab678
Anjali-60 <vanjalivrm@gmail.com>
2022-12-17 01:13:15 +0530

    stack

---
+++ learning/D_QUEUE/6_largest_rect_histogram.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int maxArea(int a[],int n)
+{
+  
+    int i = 0;
+    int ans = 0;
+    stack<int> s;
+    while (i < n)
+    {
+       // s.push(0);
+        while (!s.empty() && a[s.top()] > a[i])
+        {
+            int t = s.top();
+            int h = a[t];
+            s.pop();
+            if (s.empty())
+            {
+                ans = max(ans, h * i);
+            }
+            else
+            {
+                int len = i - s.top() - 1;
+                ans = max(ans,len*h);
+            }
+        }
+        s.push(i);
+        i++;
+    }
+    return ans;
+}
+
+int main()
+{
+    int a[] = {2, 1, 5, 6, 2, 3};
+      int n = sizeof(a)/sizeof(a[0]);
+    cout << maxArea(a,n);
+
+    return 0;
+}
